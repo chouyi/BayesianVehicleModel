29c0097e812e9956a269423e1335ee4fb6a80e8b
“chouyi” <yich8573@colorado.edu>
2020-02-23 12:43:57 -0700

    edit

--- dynamic.cpp
+++ dynamic.cpp
@@ -9,7 +9,7 @@ vector<double> CTmodel_dynamic(vector<double> cur_x,vector<double> theta,double 
     if(W==0){
         cout<<"error:omega=0"<<endl;
     }
-    next_x[0]=cur_x[0] + sin(WT)/W*cur_x[1] + (1-cos(WT))/W*cur_x[3];
+    next_x[0]=cur_x[0] + sin(WT)/W*cur_x[1] - (1-cos(WT))/W*cur_x[3];
     next_x[1]= cos(WT)*cur_x[1] -sin(WT)*cur_x[3];
     next_x[2]=(1-cos(WT))/W*cur_x[1] + cur_x[2] + sin(WT)/W*cur_x[3];
     next_x[3]= sin(WT)*cur_x[1] + cos(WT)*cur_x[3];
